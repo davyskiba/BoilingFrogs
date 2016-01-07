2d4ee60dc6a22ebace8dfcb82223bfd22e79f8e8
Wojciech Dawiskiba <w.dawiskiba@gmail.com>
2016-01-07 22:02:52 +0100

    schedule tabs implemented

--- app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/schedule/ScheduleFragment.java
@@ -3,6 +3,7 @@ package com.panoprogramowanie.boilingfrogs.ui.schedule;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,17 +23,58 @@ public class ScheduleFragment extends Fragment {
     @Bind(R.id.view_pager)
     ViewPager viewPager;
 
+    @Bind(R.id.tab_layout)
+    TabLayout tabLayout;
+
     ScheduleFragmentPagerAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View result=inflater.inflate(R.layout.fragment_schedule,container,false);
-        ButterKnife.bind(this,result);
+        View result=inflater.inflate(R.layout.fragment_schedule, container, false);
+        ButterKnife.bind(this, result);
 
-        adapter=new ScheduleFragmentPagerAdapter(getFragmentManager(), SpeechSlots.values());
+        SpeechSlots[] slots=SpeechSlots.values();
+
+        adapter=new ScheduleFragmentPagerAdapter(getChildFragmentManager(), slots);
         viewPager.setAdapter(adapter);
 
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        addTabs(tabLayout, slots,0);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
         return result;
+    }
+
+    private void addTabs(TabLayout tabLayout, SpeechSlots[] slots, int selectedPosition)
+    {
+        for(int i=0;i<slots.length;i++)
+        {
+            TabLayout.Tab tab=tabLayout.newTab().setText(slots[i].getLabelId());
+            if(i==selectedPosition) {
+                tab.select();
+            }
+            tabLayout.addTab(tab);
+        }
     }
 }
--- app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/schedule/ScheduleSlotFragment.java
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/schedule/ScheduleSlotFragment.java
@@ -6,10 +6,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.panoprogramowanie.boilingfrogs.R;
 import com.panoprogramowanie.boilingfrogs.model.SpeechSlots;
+
+import butterknife.Bind;
 
 /**
  * Created by Wojciech on 07.01.2016.
@@ -18,8 +21,7 @@ public class ScheduleSlotFragment extends Fragment {
 
     private static String SLOT_ARG_KEY="slot_arg";
 
-    public static ScheduleSlotFragment createInstance(SpeechSlots slot)
-    {
+    public static ScheduleSlotFragment createInstance(SpeechSlots slot){
         Bundle args=new Bundle();
         args.putSerializable(SLOT_ARG_KEY, slot);
 
@@ -29,15 +31,27 @@ public class ScheduleSlotFragment extends Fragment {
         return result;
     }
 
+    @Bind(R.id.button)
+    Button button;
+
+    @Bind(R.id.textView)
+    TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View result=inflater.inflate(R.layout.fragment_schedule_slot,container,false);
 
-        TextView tv=(TextView)result.findViewById(R.id.textView);
         SpeechSlots slot=(SpeechSlots)getArguments().getSerializable(SLOT_ARG_KEY);
 
-        tv.setText(slot.getLabelId());
+        textView.setText(slot.getLabelId());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return result;
     }
