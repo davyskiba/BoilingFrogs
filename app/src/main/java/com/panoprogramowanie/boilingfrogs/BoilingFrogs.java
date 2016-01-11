5222e48cce5608e873d28be1079867aa1a154e59
Wojciech Dawiskiba <w.dawiskiba@gmail.com>
2016-01-11 21:56:55 +0100

    schedule speeches list implemented

--- app/src/main/java/com/panoprogramowanie/boilingfrogs/BoilingFrogs.java
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/BoilingFrogs.java
@@ -2,8 +2,26 @@ package com.panoprogramowanie.boilingfrogs;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 /**
  * Created by Wojciech on 30.12.2015.
  */
 public class BoilingFrogs extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(this);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+    }
 }
