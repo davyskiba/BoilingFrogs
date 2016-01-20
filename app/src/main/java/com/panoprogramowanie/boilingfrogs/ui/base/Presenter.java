95225e67124ed48c9f16d5a44727ed15da632408
Wojciech Dawiskiba <w.dawiskiba@gmail.com>
2016-01-20 21:09:29 +0100

    SpeechPresenter extracted

---
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/base/Presenter.java
@@ -0,0 +1,28 @@
+package com.panoprogramowanie.boilingfrogs.ui.base;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by Wojciech on 20.01.2016.
+ */
+public class Presenter<T extends MvpView> {
+
+    private WeakReference<T> view;
+
+    public void takeView(T view)
+    {
+        this.view=new WeakReference<T>(view);
+    }
+
+    protected T getView()
+    {
+        return view.get();
+    }
+
+    protected Context getContext() {
+        return getView().getContext();
+    }
+}
---
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/ui/speech/SpeechPresenter.java
@@ -0,0 +1,68 @@
+package com.panoprogramowanie.boilingfrogs.ui.speech;
+
+import android.content.Context;
+
+import com.panoprogramowanie.boilingfrogs.R;
+import com.panoprogramowanie.boilingfrogs.model.Speech;
+import com.panoprogramowanie.boilingfrogs.model.SpeechSlot;
+import com.panoprogramowanie.boilingfrogs.suppliers.NotificationSupplier;
+import com.panoprogramowanie.boilingfrogs.suppliers.ScheduleSupplier;
+import com.panoprogramowanie.boilingfrogs.ui.base.Presenter;
+
+/**
+ * Created by Wojciech on 20.01.2016.
+ */
+public class SpeechPresenter extends Presenter<SpeechActivity>{
+
+    private final ScheduleSupplier scheduleSupplier;
+    private final NotificationSupplier notificationSupplier;
+
+    private SpeechSlot speechSlot;
+    private Speech speech;
+
+    private int speechSlotPosition;
+    private int speechPath;
+
+    public SpeechPresenter(ScheduleSupplier scheduleSupplier, NotificationSupplier notificationSupplier) {
+        this.scheduleSupplier = scheduleSupplier;
+        this.notificationSupplier = notificationSupplier;
+    }
+
+    public void setSpeech(int speechSlotPosition, int speechPath){
+        this.speechSlotPosition=speechSlotPosition;
+        this.speechPath=speechPath;
+
+        speechSlot=scheduleSupplier.getSpeechSlotForPosition(speechSlotPosition);
+        speech=speechSlot.getSpeechForPath(speechPath);
+
+        SpeechActivity view=getView();
+        view.displaySpeech(speech);
+        view.displaySpeakerData(speech.getSpeaker());
+        view.displayFavorite(isSpeechFavorite());
+    }
+
+    public boolean isSpeechFavorite(){
+        return speechSlot.getFavoriteSpeechPath()==speech.getPath();
+    }
+
+    public void favoriteClicked() {
+        if(isSpeechFavorite())
+        {
+            speechSlot.setFavoriteSpeechPath(-1);
+            displaySnackbar(R.string.speech_removed_from_favorites);
+        }
+        else {
+            speechSlot.setFavoriteSpeechPath(speech.getPath());
+            displaySnackbar(R.string.speech_added_to_favorites);
+        }
+
+        getView().displayFavorite(isSpeechFavorite());
+        
+        scheduleSupplier.speechSlotsFavoritesUpdated(getContext());
+    }
+
+    private void displaySnackbar(int textId)
+    {
+        notificationSupplier.showLongLastingSnackbar(getView().getContainerView(),textId);
+    }
+}
