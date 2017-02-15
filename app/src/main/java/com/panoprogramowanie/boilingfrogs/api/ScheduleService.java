975b311b927d9dae1fe6afe24629953ef5beda1c
Wojciech Dawiskiba <wojciech.dawiskiba@intive.com>
2017-02-15 18:58:00 +0100

    schedule source changed

--- app/src/main/java/com/panoprogramowanie/boilingfrogs/api/ScheduleService.java
+++ app/src/main/java/com/panoprogramowanie/boilingfrogs/api/ScheduleService.java
@@ -1,11 +1,9 @@
 package com.panoprogramowanie.boilingfrogs.api;
 
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import rx.Observable;
 
 public interface ScheduleService {
-    @GET("/schedule")
+    @GET("agenda.json")
     Observable<Schedule> getSchedule();
 }
